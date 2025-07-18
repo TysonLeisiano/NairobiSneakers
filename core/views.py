7304778ab14650431df5ee4c61695ed89d5038c1
unknown <tysonleisiano@gmail.com>
2025-07-19 02:47:22 +0300

    polished user registration and login functionalities

--- core/views.py
+++ core/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import AbstractBaseUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 
 from .models import Product, Customer, Category
@@ -110,7 +111,11 @@ class LoginView(APIView):
             )
 
         # ✅ Successful login logic
-        return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            "token": token.key,
+            "username": user.email
+        }, status=status.HTTP_200_OK)
     
 
 @api_view(['GET'])
